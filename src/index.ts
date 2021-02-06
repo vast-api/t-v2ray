15d7d1aadd87bff117b03877592d999cbd2edba9
GeekTR <geektheripper@gmail.com>
2021-02-06 23:14:33 +0800

    v2ray types complete

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as V2RayConf } from "../types/v2ray-conf";
+export { default as V2RayProtocol } from "../types/v2ray-protocol";
+export {
+  default as V2RayTransport,
+  V2RayStreamSettings,
+} from "../types/v2ray-transport";
